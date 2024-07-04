89dc4f8d897e1549bbfa55dbc3b7f297da022edf
ceitine <ceitinee@gmail.com>
2024-07-05 01:20:59 +0300

    current progress

---
+++ Code/DebugOverlay.Base.cs
@@ -0,0 +1,51 @@
+﻿global using Sandbox;
+global using System;
+global using System.Collections.Generic;
+
+/// <summary>
+/// Badass library for rendering debug information.
+/// </summary>
+public sealed partial class DebugOverlay : GameObjectSystem
+{
+	private static List<(
+		Action Callback,
+		TimeSince SinceCreated,
+		float Duration
+	)> _queue = new();
+
+	public DebugOverlay( Scene scene ) : base( scene )
+	{
+		_queue.Clear();
+		Listen( Stage.FinishUpdate, 0, RenderQueue, "DebugOverlay" );
+	}
+
+	private static void RenderQueue()
+	{
+		for ( int i = 0; i < _queue.Count; i++ )
+		{
+			var obj = _queue[i];
+			if ( obj.SinceCreated >= obj.Duration )
+			{
+				_queue.RemoveAt( i );
+				continue;
+			}
+
+			obj.Callback();
+		}
+	}
+
+	private static void AddToQueue( Action callback, float time )
+	{
+		if ( time <= 0f )
+		{
+			callback();
+			return;
+		}
+
+		_queue.Add( (
+			Callback: callback,
+			SinceCreated: 0f,
+			Duration: time
+		) );
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DebugOverlay
+### DebugOverlay
+
+A shrimple implementation of the old DebugOverlay from the Entity System era...
+
+[**sbox.game**](sbox.game/fish/debugoverlay)
